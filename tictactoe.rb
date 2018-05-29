746de0a126da15d3bbdf9b1a8c80a9272ab9d9af
Romane <romane71193@gmail.com>
2018-05-29 17:56:07 -0400

    centered screen & fix draw method

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,11 +1,13 @@
 class Tictactoe
 
 attr_accessor :board,:turn
+
 puts  %{
   ||T |||i |||c |||- |||T |||a |||c |||- |||T |||o |||e ||
   ||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__||
   |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
 }
+
 
 def initialize()
 
@@ -20,15 +22,15 @@ def play
 
 
 
-
-puts "Welcome to tictactoe"
 puts
-puts " #{board[0]} | #{board[1]} | #{board[2]}"
-puts "------------"
-puts " #{board[3]} | #{board[4]} | #{board[5]}"
-puts "------------"
-puts " #{board[6]} | #{board[7]} | #{board[8]}"
-
+puts "\t \t   Welcome to tictactoe"
+puts
+puts "\t \t \t #{board[0]} | #{board[1]} | #{board[2]}"
+puts " \t \t  \t ---------"
+puts "\t \t \t #{board[3]} | #{board[4]} | #{board[5]}"
+puts "\t \t \t ---------"
+puts "\t \t \t #{board[6]} | #{board[7]} | #{board[8]}"
+puts
 
 
 
@@ -37,26 +39,26 @@ until turn == 9
 
 #player == 'O' ? player = 'X' : player = 'O'
 turn.even? ?  player = 'X' : player = 'O'
-puts "Player #{player} Enter a square (1-9)"
+puts "\t \t Player #{player} Enter a square (1-9)"
 input = gets.chomp.to_i - 1
 
 if input > 10 || input == -1
-puts "Invaid,please enter a number between 1 and 9"
+puts "\t \t Invaid,please enter a number between 1 and 9"
 
 elsif board[input] == 'X' || board[input] == 'O'
-puts "Square has already been taken!"
+puts "\t \t Square has already been taken!"
 
 else
 board[input] = "#{player}"
 @turn += 1
-
+draw
 end
 
-puts " #{board[0]} | #{board[1]} | #{board[2]}"
-puts "------------"
-puts " #{board[3]} | #{board[4]} | #{board[5]}"
-puts "------------"
-puts " #{board[6]} | #{board[7]} | #{board[8]}"
+puts "\t \t \t #{board[0]} | #{board[1]} | #{board[2]}"
+puts " \t \t  \t ---------"
+puts "\t \t \t #{board[3]} | #{board[4]} | #{board[5]}"
+puts "\t \t \t ---------"
+puts "\t \t \t #{board[6]} | #{board[7]} | #{board[8]}"
 
 if board[0] == board[1] && board[0] == board[2] ||
  board[3] == board[4] && board[3] == board[5] ||
@@ -68,7 +70,7 @@ if board[0] == board[1] && board[0] == board[2] ||
 board[2] == board[4] && board[2] == board[6]
 
 
-puts "Game over #{player} won"
+puts "\t \t Game over #{player} won"
 break
 end
 
@@ -76,14 +78,19 @@ end
 
 #win_combination = [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 
-def self.draw
-  if board.all? == "X" || "O"
- puts "Game Over! Draw!"
- end
+
+
+end
+
+def draw
+if turn == 9
+p "Game Over! Draw!"
+
+end
 end
 
 
 end
-end
+
 g = Tictactoe.new
 g.play
